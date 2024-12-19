bde3d5286288a6e93c6709c3570b3c20987f7b06
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-12-19 13:47:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import vector
 from obj_reader import ObjReader
 from point import Point
+from vector import Vector
 # Divirtam-se :)
 
 def main():
@@ -7,7 +9,16 @@ def main():
     h = Point(0, 0, 0)
     c = w.point_sum(w)
     j = Point(-2, -2, -2).point_subtraction(c)
-
+    v1 = Vector(-2, 0, 1)
+    v2 = Vector(4, 2, 5)
+    print(type(v2))
+    if type(v2) == vector.Vector:
+        print('yes')
+    print(v1.vector_sum(v2))
+    print(v1.vector_subtraction(v2))
+    print(v1.vector_dot_product(v2))
+    print(v1.vector_x_scalar(2))
+    print(v1.vector_product(v2))
     print(h.point_distance(w))
     print(j.__str__())
 
