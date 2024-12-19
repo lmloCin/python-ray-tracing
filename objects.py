dbbc7a80a655e2137409e2cd5138f3188558de9b
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-12-19 13:46:38 -0300

    Create objects.py

---
+++ objects.py
@@ -0,0 +1,17 @@
+from vector import Vector
+from point import Point
+
+
+def normalize_list(list_):  # Pode ser utilizada para normalizar pontos, vetores e listas contendo valores RGB
+    p = Point(list_[0], list_[1], list_[2])
+    p0 = Point(0, 0, 0)
+    delta = p.point_distance(p0)
+    return [p.x/delta, p.y/delta, p.z/delta]
+
+
+class Sphere:
+    def __init__(self, center: Point, radius, color):
+        self.center = center  # Ponto
+        self.radius = radius  # Número real
+        self.color = color    # Lista normalizada RGB
+
