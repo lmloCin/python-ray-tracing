8a64db417bdaf03f5b67141c4d7a05647bd708b0
Lucas de Melo Lima Oliveira <112504140+lmloCin@users.noreply.github.com>
2024-12-19 11:38:58 -0300

    Update point.py

--- point.py
+++ point.py
@@ -8,13 +8,22 @@ class Point:
         z (float): Coordenada Z do ponto.
     """
 
-    def __init__(self, x: float, y: float, z:float):
+    def __init__(self, x: float, y: float, z: float):
         self.x = x
         self.y = y
         self.z = z
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
-    
-    # Implemente os métodos de pontos aqui
-    
+
+    def point_sum(self, p2):
+        return Point(self.x + p2.x, self.y + p2.y, self.z + p2.z)
+
+    def point_subtraction(self, p2):
+        return Point(self.x - p2.x, self.y - p2.y, self.z - p2.z)
+
+    def point_distance(self, p2):
+        delx = self.x - p2.x
+        dely = self.y - p2.y
+        delz = self.z - p2.z
+        return (delx ** 2 + dely ** 2 + delz ** 2)**(1/2)
