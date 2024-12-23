9a6752008c60fd41143ba8a82a66872ae1a150a3
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2024-12-23 10:55:43 -0300

    Bug fix: correção de normalização de vetores

--- vector.py
+++ vector.py
@@ -33,9 +33,9 @@ class Vector:
     def vector_return_list(self):
         return [self.x, self.y, self.z]
 
-def vector_normalize(self):
-    delta = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
-    if delta == 0:
-        raise ValueError("Não é possível normalizar um vetor nulo")
-    return Vector(self.x/delta, self.y/delta, self.z/delta)
+    def vector_normalize(self):
+        delta = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+        if delta == 0:
+            raise ValueError("Não é possível normalizar um vetor nulo")
+        return Vector(self.x/delta, self.y/delta, self.z/delta)
 
